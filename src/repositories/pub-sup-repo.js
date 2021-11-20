62cab18e3398a0869756552db18703476f3683d5
thedevyansh <schawla333333@gmail.com>
2021-11-20 11:05:16 +0530

    Add publishMessage code

---
+++ src/repositories/pub-sup-repo.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publishMessage: async (pubSubClient, topicName, payload) => {
+    const dataBuffer = Buffer.from(payload);
+
+    const messageId = await pubSubClient.topic(topicName).publish(dataBuffer);
+    console.log(`Message ${messageId} published to topic ${topicName}.`);
+    return messageId;
+  },
+};
